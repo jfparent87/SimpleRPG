799bcdce12de453a426c40a30414a28476b1aaf8
jfparent87 <jean-francois.parent.5@ulaval.ca>
2017-01-06 22:41:20 -0500

    Added weapons

--- Assets/Scripts/BaseStat.cs
+++ Assets/Scripts/BaseStat.cs
@@ -22,12 +22,13 @@ public class BaseStat {
     }
 
     public void RemoveStatBonus(StatBonus statBonus) {
-        this.BaseAdditives.Remove(statBonus);
+        this.BaseAdditives.Remove(BaseAdditives.Find(x => x.BonusValue == statBonus.BonusValue));
     }
 
     public int GetCalculatedStatValue() {
+        this.FinalValue = 0;
         this.BaseAdditives.ForEach(x => this.FinalValue += x.BonusValue);
-        FinalValue += BaseValue;
+        this.FinalValue += BaseValue;
         return FinalValue;
     }
 }
